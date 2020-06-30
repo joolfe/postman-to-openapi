05080d99214db91bdc01a12ecfb6560b05a05efb
Jorge <joolfe04@gmail.com>
2020-06-30 19:55:34 +0200

    feat: basic structure conversion

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,9 +2,17 @@
 
 const { describe, it } = require('mocha')
 const postmanToOpenApi = require('../lib')
+const path = require('path')
 
 describe('First test', function () {
-  it('test', function () {
-    postmanToOpenApi('', '', {})
+  it('should work with a basic transform', async function () {
+    const input = path.join(__dirname, '/resources/PostmantoOpenAPI.postman_collection.json')
+    const output = path.join(__dirname, '/openAPIRes.yml')
+    await postmanToOpenApi(input, output, {})
+  })
+
+  it('should work when no save', async function () {
+    const input = path.join(__dirname, '/resources/PostmantoOpenAPI.postman_collection.json')
+    const result = await postmanToOpenApi(input, '', { save: false })
   })
 })
