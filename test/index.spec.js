1d9da470d428b3053761464b7965b294c833e70b
Jorge <joolfe04@gmail.com>
2020-10-01 17:01:14 +0200

    feat: support postman collection v2
    Close #20

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,22 +8,7 @@ const { readFileSync, existsSync, unlinkSync } = require('fs')
 
 const OUTPUT_PATH = path.join(__dirname, '/openAPIRes.yml')
 
-const COLLECTION_BASIC = './test/resources/input/PostmantoOpenAPI.json'
-const COLLECTION_SIMPLE = './test/resources/input/SimplePost.json'
-const COLLECTION_NO_VERSION = './test/resources/input/NoVersion.json'
-const COLLECTION_FOLDERS = './test/resources/input/FolderCollection.json'
-const COLLECTION_GET = './test/resources/input/GetMethods.json'
-const COLLECTION_HEADERS = './test/resources/input/Headers.json'
-const COLLECTION_AUTH_BEARER = './test/resources/input/AuthBearer.json'
-const COLLECTION_AUTH_BASIC = './test/resources/input/AuthBasic.json'
-const COLLECTION_PATH_PARAMS = './test/resources/input/PathParams.json'
-const COLLECTION_MULTIPLE_SERVERS = './test/resources/input/MultipleServers.json'
-const COLLECTION_LICENSE_CONTACT = './test/resources/input/LicenseContact.json'
-const COLLECTION_DEPTH_PATH_PARAMS = './test/resources/input/DepthPathParams.json'
-const COLLECTION_PARSE_STATUS_CODE = './test/resources/input/ParseStatusCode.json'
-const COLLECTION_NO_PATH = './test/resources/input/NoPath.json'
 const COLLECTION_NO_OPTIONS = './test/resources/input/NoOptionsInBody.json'
-const COLLECTION_DELETE = './test/resources/input/DeleteOperation.json'
 
 const EXPECTED_BASIC = readFileSync('./test/resources/output/Basic.yml', 'utf8')
 const EXPECTED_INFO_OPTS = readFileSync('./test/resources/output/InfoOpts.yml', 'utf8')
@@ -55,192 +40,214 @@ describe('Library specs', function () {
     }
   })
 
-  it('should work with a basic transform', async function () {
-    const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH, {})
-    equal(result, EXPECTED_BASIC)
-    ok(existsSync(OUTPUT_PATH))
-  })
+  const TEST_VERSIONS = ['v2', 'v21']
 
-  it('should work when no save', async function () {
-    await postmanToOpenApi(COLLECTION_BASIC, null)
-  })
+  TEST_VERSIONS.forEach(function (version) {
+    describe(`Postman Collection ${version}`, function () {
+      const COLLECTION_BASIC = `./test/resources/input/${version}/PostmantoOpenAPI.json`
+      const COLLECTION_SIMPLE = `./test/resources/input/${version}/SimplePost.json`
+      const COLLECTION_NO_VERSION = `./test/resources/input/${version}/NoVersion.json`
+      const COLLECTION_FOLDERS = `./test/resources/input/${version}/FolderCollection.json`
+      const COLLECTION_GET = `./test/resources/input/${version}/GetMethods.json`
+      const COLLECTION_HEADERS = `./test/resources/input/${version}/Headers.json`
+      const COLLECTION_PATH_PARAMS = `./test/resources/input/${version}/PathParams.json`
+      const COLLECTION_MULTIPLE_SERVERS = `./test/resources/input/${version}/MultipleServers.json`
+      const COLLECTION_LICENSE_CONTACT = `./test/resources/input/${version}/LicenseContact.json`
+      const COLLECTION_DEPTH_PATH_PARAMS = `./test/resources/input/${version}/DepthPathParams.json`
+      const COLLECTION_PARSE_STATUS_CODE = `./test/resources/input/${version}/ParseStatusCode.json`
+      const COLLECTION_NO_PATH = `./test/resources/input/${version}/NoPath.json`
+      const COLLECTION_DELETE = `./test/resources/input/${version}/DeleteOperation.json`
+      const COLLECTION_AUTH_BEARER = `./test/resources/input/${version}/AuthBearer.json`
+      const COLLECTION_AUTH_BASIC = `./test/resources/input/${version}/AuthBasic.json`
 
-  it('should work if info is passed as parameter', async function () {
-    const result = await postmanToOpenApi(COLLECTION_SIMPLE, OUTPUT_PATH, {
-      info: {
-        title: 'Options title',
-        version: '6.0.7-beta',
-        description: 'Description from options',
-        termsOfService: 'http://tos.myweb.com'
-      }
+      it('should work with a basic transform', async function () {
+        const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH, {})
+        equal(result, EXPECTED_BASIC)
+        ok(existsSync(OUTPUT_PATH))
+      })
+
+      it('should work when no save', async function () {
+        await postmanToOpenApi(COLLECTION_BASIC, null)
+      })
+
+      it('should work if info is passed as parameter', async function () {
+        const result = await postmanToOpenApi(COLLECTION_SIMPLE, OUTPUT_PATH, {
+          info: {
+            title: 'Options title',
+            version: '6.0.7-beta',
+            description: 'Description from options',
+            termsOfService: 'http://tos.myweb.com'
+          }
+        })
+        equal(result, EXPECTED_INFO_OPTS)
+      })
+
+      it('should use "defaultTag" provided by config', async function () {
+        const result = await postmanToOpenApi(COLLECTION_SIMPLE, OUTPUT_PATH, { defaultTag: 'Custom Tag' })
+        equal(result, EXPECTED_CUSTOM_TAG)
+      })
+
+      it('should use default version if not informed and not in postman variables', async function () {
+        const result = await postmanToOpenApi(COLLECTION_NO_VERSION, OUTPUT_PATH, {})
+        equal(result, EXPECTED_NO_VERSION)
+      })
+
+      it('should work with folders and use as tags', async function () {
+        const result = await postmanToOpenApi(COLLECTION_FOLDERS, OUTPUT_PATH)
+        equal(result, EXPECTED_FOLDERS)
+      })
+
+      it('should parse GET methods with query string', async function () {
+        const result = await postmanToOpenApi(COLLECTION_GET, OUTPUT_PATH)
+        equal(result, EXPECTED_GET_METHODS)
+      })
+
+      it('should parse HEADERS parameters', async function () {
+        const result = await postmanToOpenApi(COLLECTION_HEADERS, OUTPUT_PATH)
+        equal(result, EXPECTED_HEADERS)
+      })
+
+      it('should parse path params', async function () {
+        const result = await postmanToOpenApi(COLLECTION_PATH_PARAMS, OUTPUT_PATH)
+        equal(result, EXPECTED_PATH_PARAMS)
+      })
+
+      it('should parse servers from existing host in postman collection', async function () {
+        const result = await postmanToOpenApi(COLLECTION_MULTIPLE_SERVERS, OUTPUT_PATH)
+        equal(result, EXPECTED_MULTIPLE_SERVERS)
+      })
+
+      it('should use servers from options', async function () {
+        const result = await postmanToOpenApi(COLLECTION_MULTIPLE_SERVERS, OUTPUT_PATH, {
+          servers: [
+            {
+              url: 'https://awesome.api.sandbox.io',
+              description: 'Sandbox environment server'
+            },
+            {
+              url: 'https://awesome.api.io',
+              description: 'Production env'
+            }
+          ]
+        })
+        equal(result, EXPECTED_SERVERS_OPTIONS)
+      })
+
+      it('should allow empty servers from options', async function () {
+        const result = await postmanToOpenApi(COLLECTION_MULTIPLE_SERVERS, OUTPUT_PATH, { servers: [] })
+        equal(result, EXPECTED_NO_SERVERS)
+      })
+
+      it('should parse license and contact from variables', async function () {
+        const result = await postmanToOpenApi(COLLECTION_LICENSE_CONTACT, OUTPUT_PATH)
+        equal(result, EXPECTED_LICENSE_CONTACT)
+      })
+
+      it('should use license and contact from options', async function () {
+        const result = await postmanToOpenApi(COLLECTION_LICENSE_CONTACT, OUTPUT_PATH,
+          {
+            info: {
+              license: {
+                name: 'MIT',
+                url: 'https://es.wikipedia.org/wiki/Licencia_MIT'
+              },
+              contact: {
+                name: 'My Support',
+                url: 'http://www.api.com/support',
+                email: 'support@api.com'
+              }
+            }
+          })
+        equal(result, EXPECTED_LICENSE_CONTACT_OPT)
+      })
+
+      it('should support optional params in license and contact options', async function () {
+        const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH,
+          {
+            info: {
+              license: {
+                name: 'MIT'
+              },
+              contact: {
+                name: 'My Support'
+              }
+            }
+          })
+        equal(result, EXPECTED_LICENSE_CONTACT_PARTIAL)
+      })
+
+      it('should support optional params in license and contact options (2)', async function () {
+        const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH,
+          {
+            info: {
+              license: {
+                name: 'MIT'
+              },
+              contact: {
+                url: 'http://www.api.com/support'
+              }
+            }
+          })
+        equal(result, EXPECTED_LICENSE_CONTACT_PARTIAL_2)
+      })
+
+      it('should use depth configuration for parse paths', async function () {
+        const result = await postmanToOpenApi(COLLECTION_DEPTH_PATH_PARAMS, OUTPUT_PATH, { pathDepth: 1 })
+        equal(result, EXPECTED_DEPTH_PATH_PARAMS)
+      })
+
+      it('should parse status codes from test', async function () {
+        const result = await postmanToOpenApi(COLLECTION_PARSE_STATUS_CODE)
+        equal(result, EXPECTED_PARSE_STATUS_CODE)
+      })
+
+      it('should parse operation when no path (only domain)', async function () {
+        const result = await postmanToOpenApi(COLLECTION_NO_PATH)
+        equal(result, EXPECTED_NO_PATH)
+      })
+
+      it('should support "DELETE" operations', async function () {
+        const result = await postmanToOpenApi(COLLECTION_DELETE)
+        equal(result, EXPECTED_DELETE)
+      })
+
+      it('should parse global authorization (Bearer)', async function () {
+        const result = await postmanToOpenApi(COLLECTION_AUTH_BEARER, OUTPUT_PATH)
+        equal(result, EXPECTED_AUTH_BEARER)
+      })
+
+      it('should parse global authorization (Basic)', async function () {
+        const result = await postmanToOpenApi(COLLECTION_AUTH_BASIC, OUTPUT_PATH)
+        equal(result, EXPECTED_AUTH_BASIC)
+      })
+
+      it('should use global authorization by configuration', async function () {
+        const authDefinition = {
+          myCustomAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'A resource owner JWT',
+            description: 'My awesome authentication using bearer'
+          },
+          myCustomAuth2: {
+            type: 'http',
+            scheme: 'basic',
+            description: 'My awesome authentication using user and password'
+          },
+          notSupported: {
+            type: 'http',
+            scheme: 'digest',
+            description: 'Not supported security'
+          }
+        }
+        const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH, { auth: authDefinition })
+        equal(result, EXPECTED_BASIC_WITH_AUTH)
+      })
     })
-    equal(result, EXPECTED_INFO_OPTS)
-  })
-
-  it('should use default version if not informed and not in postman variables', async function () {
-    const result = await postmanToOpenApi(COLLECTION_NO_VERSION, OUTPUT_PATH, {})
-    equal(result, EXPECTED_NO_VERSION)
-  })
-
-  it('should use "defaultTag" provided by config', async function () {
-    const result = await postmanToOpenApi(COLLECTION_SIMPLE, OUTPUT_PATH, { defaultTag: 'Custom Tag' })
-    equal(result, EXPECTED_CUSTOM_TAG)
-  })
-
-  it('should work with folders and use as tags', async function () {
-    const result = await postmanToOpenApi(COLLECTION_FOLDERS, OUTPUT_PATH)
-    equal(result, EXPECTED_FOLDERS)
-  })
-
-  it('should parse GET methods with query string', async function () {
-    const result = await postmanToOpenApi(COLLECTION_GET, OUTPUT_PATH)
-    equal(result, EXPECTED_GET_METHODS)
-  })
-
-  it('should parse HEADERS parameters', async function () {
-    const result = await postmanToOpenApi(COLLECTION_HEADERS, OUTPUT_PATH)
-    equal(result, EXPECTED_HEADERS)
-  })
-
-  it('should parse global authorization (Bearer)', async function () {
-    const result = await postmanToOpenApi(COLLECTION_AUTH_BEARER, OUTPUT_PATH)
-    equal(result, EXPECTED_AUTH_BEARER)
-  })
-
-  it('should parse global authorization (Basic)', async function () {
-    const result = await postmanToOpenApi(COLLECTION_AUTH_BASIC, OUTPUT_PATH)
-    equal(result, EXPECTED_AUTH_BASIC)
-  })
-
-  it('should use global authorization by configuration', async function () {
-    const authDefinition = {
-      myCustomAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'A resource owner JWT',
-        description: 'My awesome authentication using bearer'
-      },
-      myCustomAuth2: {
-        type: 'http',
-        scheme: 'basic',
-        description: 'My awesome authentication using user and password'
-      },
-      notSupported: {
-        type: 'http',
-        scheme: 'digest',
-        description: 'Not supported security'
-      }
-    }
-    const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH, { auth: authDefinition })
-    equal(result, EXPECTED_BASIC_WITH_AUTH)
-  })
-
-  it('should parse path params', async function () {
-    const result = await postmanToOpenApi(COLLECTION_PATH_PARAMS, OUTPUT_PATH)
-    equal(result, EXPECTED_PATH_PARAMS)
-  })
-
-  it('should parse servers from existing host in postman collection', async function () {
-    const result = await postmanToOpenApi(COLLECTION_MULTIPLE_SERVERS, OUTPUT_PATH)
-    equal(result, EXPECTED_MULTIPLE_SERVERS)
-  })
-
-  it('should use servers from options', async function () {
-    const result = await postmanToOpenApi(COLLECTION_MULTIPLE_SERVERS, OUTPUT_PATH, {
-      servers: [
-        {
-          url: 'https://awesome.api.sandbox.io',
-          description: 'Sandbox environment server'
-        },
-        {
-          url: 'https://awesome.api.io',
-          description: 'Production env'
-        }
-      ]
-    })
-    equal(result, EXPECTED_SERVERS_OPTIONS)
-  })
-
-  it('should allow empty servers from options', async function () {
-    const result = await postmanToOpenApi(COLLECTION_MULTIPLE_SERVERS, OUTPUT_PATH, { servers: [] })
-    equal(result, EXPECTED_NO_SERVERS)
-  })
-
-  it('should parse license and contact from variables', async function () {
-    const result = await postmanToOpenApi(COLLECTION_LICENSE_CONTACT, OUTPUT_PATH)
-    equal(result, EXPECTED_LICENSE_CONTACT)
-  })
-
-  it('should use license and contact from options', async function () {
-    const result = await postmanToOpenApi(COLLECTION_LICENSE_CONTACT, OUTPUT_PATH,
-      {
-        info: {
-          license: {
-            name: 'MIT',
-            url: 'https://es.wikipedia.org/wiki/Licencia_MIT'
-          },
-          contact: {
-            name: 'My Support',
-            url: 'http://www.api.com/support',
-            email: 'support@api.com'
-          }
-        }
-      })
-    equal(result, EXPECTED_LICENSE_CONTACT_OPT)
-  })
-
-  it('should support optional params in license and contact options', async function () {
-    const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH,
-      {
-        info: {
-          license: {
-            name: 'MIT'
-          },
-          contact: {
-            name: 'My Support'
-          }
-        }
-      })
-    equal(result, EXPECTED_LICENSE_CONTACT_PARTIAL)
-  })
-
-  it('should support optional params in license and contact options (2)', async function () {
-    const result = await postmanToOpenApi(COLLECTION_BASIC, OUTPUT_PATH,
-      {
-        info: {
-          license: {
-            name: 'MIT'
-          },
-          contact: {
-            url: 'http://www.api.com/support'
-          }
-        }
-      })
-    equal(result, EXPECTED_LICENSE_CONTACT_PARTIAL_2)
-  })
-
-  it('should use depth configuration for parse paths', async function () {
-    const result = await postmanToOpenApi(COLLECTION_DEPTH_PATH_PARAMS, OUTPUT_PATH, { pathDepth: 1 })
-    equal(result, EXPECTED_DEPTH_PATH_PARAMS)
-  })
-
-  it('should parse status codes from test', async function () {
-    const result = await postmanToOpenApi(COLLECTION_PARSE_STATUS_CODE)
-    equal(result, EXPECTED_PARSE_STATUS_CODE)
-  })
-
-  it('should parse operation when no path (only domain)', async function () {
-    const result = await postmanToOpenApi(COLLECTION_NO_PATH)
-    equal(result, EXPECTED_NO_PATH)
   })
 
   it('should work if no options in request body', async function () {
     const result = await postmanToOpenApi(COLLECTION_NO_OPTIONS, OUTPUT_PATH, {})
     equal(result, EXPECTED_BASIC)
-  })
-
-  it('should support "DELETE" operations', async function () {
-    const result = await postmanToOpenApi(COLLECTION_DELETE)
-    equal(result, EXPECTED_DELETE)
   })
 })
