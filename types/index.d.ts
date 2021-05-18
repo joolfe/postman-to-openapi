36cf16bdf6659997f8c068e8cfdba3943d9d6c2e
Jorge <joolfe04@gmail.com>
2021-05-18 18:16:23 +0200

    feat: add ts definitions to library

---
+++ types/index.d.ts
@@ -0,0 +1,83 @@
+
+/** 
+ * Object that contain the license information as defined in
+ * https://spec.openapis.org/oas/v3.0.3.html#contactObject
+ */
+export interface ContactObject {
+    name?: string,
+    url?: string,
+    email?: string
+}
+
+/** 
+ * Object that contain the license information as defined in
+ * https://spec.openapis.org/oas/v3.0.3.html#licenseObject
+ */
+export interface LicenseObject {
+    name: string,
+    url?: string
+}
+
+/** 
+ * Object that contain the api information as defined in
+ * https://spec.openapis.org/oas/v3.0.3.html#info-object
+ */
+export interface InfoObject {
+    title?: string,
+    description?: string,
+    termsOfService?: string,
+    contact?: ContactObject,
+    license?: LicenseObject,
+    version?: string,
+}
+
+/**
+ * Object that contain the servers information as defined in
+ * https://spec.openapis.org/oas/v3.0.3.html#server-object
+ * field `variables`is not supported yet
+ */
+export interface ServerObject {
+    url: string,
+    description?: string
+}
+
+/**
+ * Object that contain the servers information as defined in
+ * https://spec.openapis.org/oas/v3.0.3.html#security-scheme-object
+ */
+export interface SecurityObject {
+    type: 'http',
+    description?: string,
+    scheme: 'bearer' | 'basic',
+    bearerFormat?: string
+}
+
+export interface AuthOptions {
+    [key: string]: SecurityObject
+}
+
+/**
+ * Object that contain the external docs information as defined in
+ * https://spec.openapis.org/oas/v3.0.3.html#external-documentation-object
+ */
+export interface ExternalDocsObject {
+    description?: string,
+    url?: string
+}
+
+export interface FoldersOption {
+    concat: boolean,
+    separator: string
+}
+
+export interface Options {
+    info?: InfoObject,
+    defaultTag?: string,
+    pathDepth?: number,
+    auth?: AuthOptions,
+    servers?: Array<ServerObject>,
+    externalDocs?: ExternalDocsObject,
+    folders?: FoldersOption
+}
+
+export function postmanToOpenApi (input: string, output?: string, options?: Options) : string
